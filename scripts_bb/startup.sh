ec72a7075961284db0cd71b607adcc382a0f7c2a
Pierre Parent <pierre.parent@pparent.fr>
2018-04-07 12:09:10 +0200

    Few updates

--- scripts_bb/startup.sh
+++ scripts_bb/startup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+mode=$(uci get bridgebox.general.mode)
+
 /scripts_bb/wifi.sh
 /etc/init.d/dnsmasq restart
+
+if [ "$mode" = "server" ]; then
+    /scripts_bb/server/startup-server.sh
+fi
