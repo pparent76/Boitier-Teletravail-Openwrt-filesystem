c7ede42bf5e71660954421ce56232eab5ebd1cdc
Pierre Parent <pierre.parent@pparent.fr>
2018-06-25 20:07:40 +0200

    Mise à jour des fichiers de configuration

--- etc/openvpn/server-passwords-check.sh
+++ etc/openvpn/server-passwords-check.sh
@@ -3,11 +3,18 @@
 read  username < $1
 read  password < $1
 
-# Replace your own authentication mechanism here
-if [[ "$password" == "Pierre" ]]; then
-  echo "ok"
-  exit 0
-fi
+while IFS='' read -r line || [[ -n "$line" ]]; do
+ if [ "$line" != "" ]; then 
+    code=$(echo "$line" | tr '#' '\n' | head -n 1 | tr '\n' ' ' | sed "s/ //g")  
+    # Replace your own authentication mechanism here
+    if [[ "$password" == "$code" ]]; then
+        echo "ok"
+        exit 0
+    fi 
+  fi
+done < /etc/server-codes
+
+
 
 echo "not ok"
 exit 1 
