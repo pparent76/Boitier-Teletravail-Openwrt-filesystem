e1947be13efbbf57efcfd1579a644edfc210599a
Pierre Parent <pierre.parent@pparent.fr>
2018-07-06 19:37:58 +0200

    Correction d'un bug pour l'accès à l'interface d'aministration (2.2.2.2)  en mode entreprise

--- scripts_bb/common-iptables.sh
+++ scripts_bb/common-iptables.sh
@@ -14,6 +14,14 @@ ebtables -t broute -A BROUTING -p ipv4 -i wlan1 --ip-dst 2.2.2.2 -j redirect --r
 ebtables -t broute -A BROUTING -p ipv4 -i eth1 --ip-dst 2.2.2.2 -j redirect --redirect-target DROP
 iptables -I FORWARD -i eth0 -s 2.2.2.2 -j DROP
 
-#ebtables -t broute -A BROUTING -p ipv4  --ip-src 2.2.2.2 -j redirect --redirect-target DROP
+ip rule add fwmark 2 table 3
+iptables -t mangle -I OUTPUT -s 2.2.2.2 -j MARK --set-mark 2
+ip route add 0.0.0.0/0 dev br-lan table 3
+ 
+
+# ebtables -t broute -A OUTPUT -p ipv4 -o tap0  --ip-src 2.2.2.2 -j redirect --redirect-target DROP
+# ebtables -t broute -A OUTPUT  -p ipv4 -o wlan1  --ip-src 2.2.2.2 -j redirect --redirect-target ACCEPT
+# ebtables -t broute -A OUTPUT  -p ipv4 -o eth1  --ip-src 2.2.2.2 -j redirect --redirect-target ACCEPT
+
 #ebtables -t broute -A BROUTING -p ipv4 -i eth1 --ip-dst 2.2.2.2 -j redirect --redirect-target DROP
 
