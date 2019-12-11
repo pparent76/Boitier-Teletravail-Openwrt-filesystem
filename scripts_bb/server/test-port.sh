3d13c77dcb93766d5112c4f96cedbafcca0c7069
Pierre Parent <pierre.parent@pparent.fr>
2019-12-11 20:21:29 +0100

    Mise à jour du script testport

--- scripts_bb/server/test-port.sh
+++ scripts_bb/server/test-port.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-randm=$(date +%s | sha256sum | head -c 16 ; echo)
-# iptables -I INPUT -m string --string 'dummy' -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random" --log-level debug
+random=$(date +%s | sha256sum | head -c 16 ; echo)
+iptables -I INPUT -m string --string 'dummy' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random" 
 
-iptables -I INPUT -m length --length 34 -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$randm"
 for i in $(seq 1 10);do
     dummyudp 1000 $2 $1
-    dmesg | grep "TEST-PORT-OK-$randm" >/dev/null 2>&1
+    dmesg | grep "TEST-PORT-OK-$random" >/dev/null 2>&1
     if [ "$?" -eq "0" ]; then
         echo "OK"
-#         iptables -D INPUT -m string --string 'dummy' -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
-        iptables -D INPUT -m length --length 34 -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$randm"
+         iptables -D INPUT -m string --string 'dummy' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
         exit;
     fi
     sleep 1;
 done
 
 echo "NO"
-#         iptables -D INPUT -m string --string 'dummy' -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
-        iptables -D INPUT -m length --length 34 -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$randm"
+         iptables -D INPUT -m string --string 'dummy' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
