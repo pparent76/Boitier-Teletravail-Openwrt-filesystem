462cba0a592fd0e88c66c41f2ff23450aef5427e
Pierre Parent <pierre.parent@pparent.fr>
2019-12-11 22:19:15 +0100

    Correction du script VPN + suppresion de fichier inutils

--- scripts_bb/server/vpn.sh
+++ scripts_bb/server/vpn.sh
@@ -20,42 +20,49 @@ ip link set up dev wg0
 ############################################################
 #          Configure the authorized wireguard clients
 ############################################################
-#wg set wg0 peer PEER_B_PUBLIC_KEY allowed-ips 10.0.0.0/24 
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+ if [ "$line" != "" ]; then 
+    clefl=$(echo "$line" | tr '#' '\n' | head -n 2 | tail -n 1 | tr '\n' ' ' | sed "s/ //g")  
+    wg set wg0 peer $clefl allowed-ips 10.0.0.0/24 
+  fi
+done < /etc/server-codes
+
 
 ############################################################
 #           We add 7 gre interfaces for the 7 clients
 ############################################################
 ip link add tap1 type gretap remote 10.0.0.2 local 10.0.0.1
 ip link set up dev tap1
-brctl addif br-lan tap1
+brctl addif br-wan tap1
 ip link set tap1 mtu 15000
 
 ip link add tap2 type gretap remote 10.0.0.3 local 10.0.0.1
 ip link set up dev tap2
-brctl addif br-lan tap2
+brctl addif br-wan tap2
 ip link set tap2 mtu 15000
 
 ip link add tap3 type gretap remote 10.0.0.4 local 10.0.0.1
 ip link set up dev tap3
-brctl addif br-lan tap3
+brctl addif br-wan tap3
 ip link set tap3 mtu 15000
 
 ip link add tap4 type gretap remote 10.0.0.5 local 10.0.0.1
 ip link set up dev tap4
-brctl addif br-lan tap4
+brctl addif br-wan tap4
 ip link set tap4 mtu 15000
 
 ip link add tap5 type gretap remote 10.0.0.6 local 10.0.0.1
 ip link set up dev tap5
-brctl addif br-lan tap5
+brctl addif br-wan tap5
 ip link set tap5 mtu 15000
 
 ip link add tap6 type gretap remote 10.0.0.7 local 10.0.0.1
 ip link set up dev tap6
-brctl addif br-lan tap6
+brctl addif br-wan tap6
 ip link set tap6 mtu 15000
 
 ip link add tap7 type gretap remote 10.0.0.8 local 10.0.0.1
 ip link set up dev tap7
-brctl addif br-lan tap7
+brctl addif br-wan tap7
 ip link set tap7 mtu 15000
