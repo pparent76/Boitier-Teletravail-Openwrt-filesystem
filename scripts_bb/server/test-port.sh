1e40634f9830a4897af11dbb824194aa38b22f33
Pierre Parent <pierre.parent@pparent.fr>
2019-12-26 23:14:53 +0100

    Correction to avoid interference between stun and testport dummyudp

--- scripts_bb/server/test-port.sh
+++ scripts_bb/server/test-port.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 random=$(date +%s | sha256sum | head -c 16 ; echo)
-iptables -I INPUT -m string --string 'dummy' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random" 
+iptables -I INPUT -m string --string 'testportsfdkjhfscbdg45' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random" 
 
 for i in $(seq 1 10);do
-    dummyudp 1000 $2 $1
+    dummyudp 1000 $2 $1 "testportsfdkjhfscbdg45"
     dmesg | grep "TEST-PORT-OK-$random" >/dev/null 2>&1
     if [ "$?" -eq "0" ]; then
         echo "OK"
-         iptables -D INPUT -m string --string 'dummy' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
+         iptables -D INPUT -m string --string 'testportsfdkjhfscbdg45' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
         exit;
     fi
     sleep 1;
 done
 
 echo "NO"
-         iptables -D INPUT -m string --string 'dummy' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
+         iptables -D INPUT -m string --string 'testportsfdkjhfscbdg45' --algo bm -p udp --dport $3 -j LOG --log-prefix "TEST-PORT-OK-$random"
