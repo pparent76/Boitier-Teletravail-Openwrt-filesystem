9acea327f7838271e793046ba0f76260b58d4973
Pierre Parent <pierre.parent@pparent.fr>
2020-01-05 01:58:52 +0100

    Correction supplémentaire pour stun

--- scripts_bb/client/stun.sh
+++ scripts_bb/client/stun.sh
@@ -53,10 +53,11 @@ rm /tmp/stunres
 
 code=$(uci get bridgebox.client.stuncode )
 
-for i in $( seq 1 3 ); do
+for k in $( seq 1 2 ); do
+for j in $( seq 1 3 ); do
 
-    stunserver=$(uci get bridgebox.advanced.stun$i)
-    stunport=$(uci get bridgebox.advanced.stunport$i)
+    stunserver=$(uci get bridgebox.advanced.stun$j)
+    stunport=$(uci get bridgebox.advanced.stunport$j)
     
     if [ "$stunserver" != "" ] && [ "$stunport" != "" ]; then
         stunclient --localport 1194 $stunserver $stunport > /tmp/stunres
@@ -71,97 +72,91 @@ for i in $( seq 1 3 ); do
         stun_localip=$(cat /tmp/stunres | grep Local | awk '{print $3}' | sed "s/:/ /g" | awk '{print $1}'  )      
         echo "Stun Port Ok $stunserver  $stunport"
         stunok=1;
-        break
-    else
-        echo "stun $stunserver $port unreachable"
-    fi
-done
 
-if [ "$stunok" -ne "1" ]; then
-    return 1;
-fi
+        log_stun "stun mapped local  $stun_mappedport $stun_localport"
 
+        if [ "$stun_publicip" = "$ip" ]; then
+            log_stun "We are behind the same nat that the other bridgebox, using local adresses"
+            #stun_publicip=192.168.1.1;
+            stun_publicip="$stun_localip";
+            stun_mappedport=$stun_localport;
+            export ip="$localip";
+        fi
 
-log_stun "stun mapped local  $stun_mappedport $stun_localport"
-
-if [ "$stun_publicip" = "$ip" ]; then
-    log_stun "We are behind the same nat that the other bridgebox, using local adresses"
-    #stun_publicip=192.168.1.1;
-    stun_publicip="$stun_localip";
-    stun_mappedport=$stun_localport;
-    export ip="$localip";
-fi
-
-log_stun "remote $ip $port"
+        log_stun "remote $ip $port"
 
 
-rm /tmp/dummyudp-server
+        rm /tmp/dummyudp-server
 
 
-serverid=$(uci get bridgebox.client.server_id )
-for i in $(seq 1 3); do
-   ##################################################################################################
-   #                                Try with hiddenservice
-   ##################################################################################################
-   killall dummyudp-server; dummyudp $stun_localport $ip $port; dummyudp-server $stun_localport >> /tmp/dummyudp-server 2>&1 & 
+        serverid=$(uci get bridgebox.client.server_id )
+        for i in $(seq 1 3); do
+            ##################################################################################################
+            #                                Try with hiddenservice
+            ##################################################################################################
+            killall dummyudp-server; dummyudp $stun_localport $ip $port; dummyudp-server $stun_localport >> /tmp/dummyudp-server 2>&1 & 
    
-   log "dummyudp-server $stun_localport"
-    torproxy=$(uci get bridgebox.advanced.torproxy)
+            log "dummyudp-server $stun_localport"
+            torproxy=$(uci get bridgebox.advanced.torproxy)
     
        
-    #Get challenge
-    wget-tor-proxy $serverid challenge | tail -n 1 > /tmp/stun-challenge
-    challengesrc=$(cat /tmp/stun-challenge)
-    challengeres=$(echo "$challengesrc" | openssl enc -aes-256-cbc -a -pass pass:$code)
-    challengeres=$(urlencode_many_printf "$challengeres")
+            #Get challenge
+            wget-tor-proxy $serverid challenge | tail -n 1 > /tmp/stun-challenge
+            challengesrc=$(cat /tmp/stun-challenge)
+            challengeres=$(echo "$challengesrc" | openssl enc -aes-256-cbc -a -pass pass:$code)
+            challengeres=$(urlencode_many_printf "$challengeres")
     
-    url="stun.sh?ip=$stun_publicip&port=$stun_mappedport&challenge=$challengeres"
-    #Ask for server push-hole
-    log_stun " wget-tor-proxy $serverid $url"
-    wget-tor-proxy $serverid $url > /tmp/stun-try-res
+            url="stun.sh?ip=$stun_publicip&port=$stun_mappedport&challenge=$challengeres"
+            #Ask for server push-hole
+            log_stun " wget-tor-proxy $serverid $url"
+            wget-tor-proxy $serverid $url > /tmp/stun-try-res
 
 
     
-    #Wait and see if we could get a dummy reply from server
-    sleep 1;
-    cat /tmp/dummyudp-server | grep dummy
-    if  [ "$?" -eq "0" ]; then
-        log_stun "Could establish stun pushhole with  $serverid (me ip=$stun_publicip\&port=$stun_mappedport) with $torproxy" 
-        log_stun "(challenge= $challengesrc)"
-        killall dummyudp-server
-        return 0;
-    fi
+            #Wait and see if we could get a dummy reply from server
+            sleep 1;
+            cat /tmp/dummyudp-server | grep dummy
+            if  [ "$?" -eq "0" ]; then
+                log_stun "Could establish stun pushhole with  $serverid (me ip=$stun_publicip\&port=$stun_mappedport) with $torproxy" 
+                log_stun "(challenge= $challengesrc)"
+                killall dummyudp-server
+                return 0;
+            fi
     
     
 
-   ##################################################################################################
-   #                                Try with torsocks
-   ##################################################################################################
-    #Send a dummmy udp package to server and record dummy reply
-    killall dummyudp-server; dummyudp $stun_localport $ip $port; dummyudp-server 1194 >> /tmp/dummyudp-server 2>&1 &
+            ##################################################################################################
+            #                                Try with torsocks
+            ##################################################################################################
+            #Send a dummmy udp package to server and record dummy reply
+            killall dummyudp-server; dummyudp $stun_localport $ip $port; dummyudp-server 1194 >> /tmp/dummyudp-server 2>&1 &
    
 
-    #wget
-     torsocks wget --retry-connrefused --waitretry=1 -t 1 $serverid.onion/challenge --timeout=20 --dns-timeout=20 --connect-timeout=20 --read-timeout=20 -O /tmp/stun-challenge > /dev/null 2>&1
-    challengesrc=$(cat /tmp/stun-challenge)
-    challengeres=$(echo "$challengesrc" | openssl enc -aes-256-cbc -a -pass pass:$code)
-    challengeres=$(urlencode_many_printf "$challengeres")
-    log_stun "http://$serverid.onion/stun.sh?ip=$stun_publicip\&port=$stun_mappedport\&challenge=$challengeres"
-    torsocks wget --retry-connrefused --waitretry=1 -t 1 $serverid.onion/stun.sh?ip=$stun_publicip\&port=$stun_mappedport\&challenge=$challengeres --timeout=20 --dns-timeout=20 --connect-timeout=20 --read-timeout=20 -O /tmp/advertised-stun-res > /dev/null 2&>1
+            #wget
+            torsocks wget --retry-connrefused --waitretry=1 -t 1 $serverid.onion/challenge --timeout=20 --dns-timeout=20 --connect-timeout=20 --read-timeout=20 -O /tmp/stun-challenge > /dev/null 2>&1
+            challengesrc=$(cat /tmp/stun-challenge)
+            challengeres=$(echo "$challengesrc" | openssl enc -aes-256-cbc -a -pass pass:$code)
+            challengeres=$(urlencode_many_printf "$challengeres")
+            log_stun "http://$serverid.onion/stun.sh?ip=$stun_publicip\&port=$stun_mappedport\&challenge=$challengeres"
+            torsocks wget --retry-connrefused --waitretry=1 -t 1 $serverid.onion/stun.sh?ip=$stun_publicip\&port=$stun_mappedport\&challenge=$challengeres --timeout=20 --dns-timeout=20 --connect-timeout=20 --read-timeout=20 -O /tmp/advertised-stun-res > /dev/null 2&>1
     
-    #Wait and see if we could get a dummy reply from server
-    sleep 1;
-    cat /tmp/dummyudp-server | grep dummy
-    if  [ "$?" -eq "0" ]; then
-        log_stun "Could establish stun pushhole with  $serverid (me ip=$stun_publicip\&port=$stun_mappedport) with torsocks"
-        log_stun "(challenge= $challengesrc)"
-        killall dummyudp-server
-        return 0;
-    fi
+            #Wait and see if we could get a dummy reply from server
+            sleep 1;
+            cat /tmp/dummyudp-server | grep dummy
+            if  [ "$?" -eq "0" ]; then
+                log_stun "Could establish stun pushhole with  $serverid (me ip=$stun_publicip\&port=$stun_mappedport) with torsocks"
+                log_stun "(challenge= $challengesrc)"
+                killall dummyudp-server
+                return 0;
+            fi
 
 
-    log_stun "New attempt to push udp hole"
-done 
+            log_stun "New attempt to push udp hole"
+
+        done
+fi
+done
+done
 
 killall dummyudp-server
 log_stun "Failed to push UDP hole"
